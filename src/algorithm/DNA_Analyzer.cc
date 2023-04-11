f58c0f890223996028ea3fca5c54d23dc5518abe
werewolf <astrodynami.fkg@gmail.com>
2023-04-11 19:57:31 +0700

    finish Nidmad Wunsh

--- src/algorithm/DNA_Analyzer.cc
+++ src/algorithm/DNA_Analyzer.cc
@@ -8,7 +8,7 @@ void DNA_Analyzer::RabinKarpAlgorithm(const std::filesystem::path& path_1, const
   const std::size_t samp_size = samp.size();
 
   std::vector<std::size_t> exp(std::max(text_size, samp_size), 1);
-  std::transform(exp.begin(), exp.end() - 1, exp.begin() + 1, [this](std::size_t &item) { return (item * rk_base) % rk_mod; });
+  std::transform(exp.begin(), exp.end() - 1, exp.begin() + 1, [this](std::size_t& item) { return (item * m_rk_base) % m_rk_mod; });
 
   std::vector<std::size_t> hash_text(text_size + 1, 0);
   CalculateMassHash(text, hash_text, text_size, exp);
@@ -17,17 +17,17 @@ void DNA_Analyzer::RabinKarpAlgorithm(const std::filesystem::path& path_1, const
   CalculateMassHash(samp, hash_samp, samp_size, exp);
 
   for (std::size_t idx = 0; idx + samp_size - 1 < text_size; ++idx) {
-    std::size_t hash_curr = (hash_text[idx + samp_size] + rk_mod - hash_text[idx]) % rk_mod;
-    if ((hash_curr == (hash_samp[samp_size] * exp[idx] % rk_mod)) && text.substr(idx, samp_size) == samp) {
+    std::size_t hash_curr = (hash_text[idx + samp_size] + m_rk_mod - hash_text[idx]) % m_rk_mod;
+    if ((hash_curr == (hash_samp[samp_size] * exp[idx] % m_rk_mod)) && text.substr(idx, samp_size) == samp) {
       std::cout << idx << " ";
     }
   }
   std::cout << std::endl;
 }
 
-void DNA_Analyzer::CalculateMassHash(const std::string &src, std::vector<std::size_t> &hash, const std::size_t size, const std::vector<std::size_t> &exp) {
+void DNA_Analyzer::CalculateMassHash(const std::string& src, std::vector<std::size_t>& hash, const std::size_t size, const std::vector<std::size_t>& exp) {
   for (std::size_t i = 0; i < size; ++i) {
-    hash[i + 1] = (hash[i] + src[i] * exp[i]) % rk_mod;
+    hash[i + 1] = (hash[i] + src[i] * exp[i]) % m_rk_mod;
   }
 }
 
@@ -37,88 +37,105 @@ std::string DNA_Analyzer::ReadFile(const std::filesystem::path& path) {
 }
 
 void DNA_Analyzer::NWAlgorithm(const std::filesystem::path& path) {
+  auto [conf, seq] = GetNWConfig(path);
+
+  InitializeWeights();
+  auto score = InitScoreMatrix(conf, 4, 4);
+  auto mat = InitAlighmentMatrix(conf, seq, score);
+  auto alig = ComputeAlignedSequences(conf, seq, mat, score);
+  PrintNWResult(alig, mat[seq[0].length()][seq[1].length()]);
+}
+
+std::pair<std::array<int, 3>, std::array<std::string, 2>> DNA_Analyzer::GetNWConfig(const std::filesystem::path& path) {
   std::ifstream file(path);
 
-  Score score(0, 0, 0);
-  file >> score.match >> score.mismatch >> score.gap;
-  std::string a, b;
-  file >> a >> b;
+  std::array<int, 3> conf;
+  std::array<std::string, 2> seq;
 
-  std::cout << optimalScore(score, a, b);
+  file >> conf[0] >> conf[1] >> conf[2];
+  file >> seq[0] >> seq[1];
 
-  auto k = optimalAlignment(score, a, b);
-  std::cout << k.first << std::endl;
-  std::cout << k.second.first << std::endl;
-  std::cout << k.second.second << std::endl;
+  file.close();
+
+  return {conf, seq};
 }
 
-int DNA_Analyzer::optimalScore(Score score, std::string s1, std::string s2) {
-  int n = s1.size(), m = s2.size();
-  std::vector<std::vector<int>> dp(n + 1, std::vector<int>(m + 1));
-  for (int i = 1; i <= n; i++) dp[i][0] = dp[i - 1][0] + score.gap;
-  for (int i = 1; i <= m; i++) dp[0][i] = dp[0][i - 1] + score.gap;
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= m; j++) {
-      int match = dp[i - 1][j - 1] +
-                  (s1[i - 1] == s2[j - 1] ? score.match : score.mismatch);
-      int ins = dp[i][j - 1] + score.gap;
-      int del = dp[i - 1][j] + score.gap;
-      dp[i][j] = std::max({match, ins, del});
+void DNA_Analyzer::InitializeWeights() {
+  for (int i = 0; i < 4; ++i) {
+    m_weights[m_ABC[i]] = i;
+  }
+}
+
+std::vector<std::vector<int> > DNA_Analyzer::InitScoreMatrix(const std::array<int, 3> &conf, const std::size_t rows, const std::size_t cols) {
+  std::vector<std::vector<int> > mat(rows, std::vector<int>(cols, 0));
+  for (std::size_t i = 0; i < rows; ++i) {
+    for (std::size_t j = 0; j < cols; ++j) {
+      mat[i][j] = ((i == j) ? conf[0] : conf[1]);
     }
   }
-  return dp[n][m];
+  return mat;
 }
 
-std::pair<int, std::pair<std::string, std::string>>
-DNA_Analyzer::optimalAlignment(Score score, std::string s1, std::string s2) {
-  int n = s1.size(), m = s2.size();
-  std::vector<std::vector<int>> dp(n + 1, std::vector<int>(m + 1));
-  std::vector<std::vector<int>> p(
-      n + 1, std::vector<int>(m + 1));  // 0-diag, 1-up, 2-left
+std::vector<std::vector<int> > DNA_Analyzer::InitAlighmentMatrix(const std::array<int, 3> &conf, const std::array<std::string, 2> &seq, const std::vector<std::vector<int> > &score_mat) {
+  const int rows = seq[0].length() + 1;
+  const int cols = seq[1].length() + 1;
+  std::vector<std::vector<int> > mat(rows, std::vector<int>(cols, 0));
 
-  for (int i = 1; i <= n; i++) dp[i][0] = dp[i - 1][0] + score.gap, p[i][0] = 1;
-  for (int i = 1; i <= m; i++) dp[0][i] = dp[0][i - 1] + score.gap, p[0][i] = 2;
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= m; j++) {
-      int match = dp[i - 1][j - 1] +
-                  (s1[i - 1] == s2[j - 1] ? score.match : score.mismatch);
-      int ins = dp[i][j - 1] + score.gap;
-      int del = dp[i - 1][j] + score.gap;
-      if (match >= ins && match >= del) {
-        dp[i][j] = match;
-        p[i][j] = 0;
-      } else if (ins >= match && ins >= del) {
-        dp[i][j] = ins;
-        p[i][j] = 2;
-      } else {
-        dp[i][j] = del;
-        p[i][j] = 1;
-      }
+  for (int i = 0; i < rows; ++i) {
+    mat[i][0] = i * conf[2];
+  }
+
+  for (int j = 0; j < cols; ++j) {
+    mat[0][j] = j * conf[2];
+  }
+
+  for (int i = 1; i < rows; ++i) {
+    const char c1 = seq[0][i - 1];
+    for (int j = 1; j < cols; ++j) {
+      const char c2 = seq[1][j - 1];
+
+      const int score1 = mat[i - 1][j - 1] + score_mat[m_weights[c1]][m_weights[c2]];
+      const int score2 = mat[i - 1][j] + conf[2];
+      const int score3 = mat[i][j - 1] + conf[2];
+
+      mat[i][j] = std::max({score1, score2, score3});
     }
   }
 
-  int i = n, j = m;
-  std::string res1, res2;
+  return mat;
+}
+
+std::array<std::string, 3> DNA_Analyzer::ComputeAlignedSequences(const std::array<int, 3> &conf, const std::array<std::string, 2> &seq, const std::vector<std::vector<int> > &mat, const std::vector<std::vector<int> > &score_mat) {
+  std::array<std::string, 3> alig;
+
+  int i = seq[0].length(), j = seq[1].length();
   while (i > 0 || j > 0) {
-    if (p[i][j] == 0) {
-      res1 += s1[i - 1];
-      res2 += s2[j - 1];
-      i--;
-      j--;
-    } else if (p[i][j] == 1) {
-      res1 += s1[i - 1];
-      res2 += '-';
-      i--;
+    if (i > 0 && j > 0 && mat[i][j] == mat[i - 1][j - 1] + score_mat[m_weights[seq[0][i - 1]]][m_weights[seq[1][j - 1]]]) {
+      alig[0] += seq[0][--i];
+      alig[2] += seq[1][--j];
+    } else if (i > 0 && mat[i][j] == mat[i - 1][j] + conf[2]) {
+      alig[0] += seq[0][--i];
+      alig[2] += '-';
     } else {
-      res1 += '-';
-      res2 += s2[j - 1];
-      j--;
+      alig[0] += '-';
+      alig[2] += seq[1][--j];
     }
+    alig[1] += alig[0].back() == alig[2].back() ? '|' : ' ';
   }
-  reverse(res1.begin(), res1.end());
-  reverse(res2.begin(), res2.end());
 
-  return {dp[n][m], {res1, res2}};
+  std::reverse(alig[0].begin(), alig[0].end());
+  std::reverse(alig[2].begin(), alig[2].end());
+  std::reverse(alig[1].begin(), alig[1].end());
+
+  return alig;
+}
+
+void DNA_Analyzer::PrintNWResult(const std::array<std::string, 3> &alig, const int &score) {
+  std::cout << "Max score: " << score << std::endl;
+
+  std::cout << alig[0] << std::endl;
+  std::cout << alig[1] << std::endl;
+  std::cout << alig[2] << std::endl;
 }
 
 void DNA_Analyzer::RegexAlgorithm(const std::filesystem::path& path) {
@@ -140,7 +157,8 @@ bool DNA_Analyzer::isMatch(std::string s, std::string p) {
       if (s[j - 1] == p[i - 1] || p[i - 1] == '.')
         dp[i][j] = dp[i - 1][j - 1];
       else if (p[i - 1] == '*') {
-        dp[i][j] = dp[i - 2][j] || (dp[i][j - 1] && (s[j - 1] == p[i - 2] || p[i - 2] == '.'));
+        dp[i][j] = dp[i - 2][j] ||
+                   (dp[i][j - 1] && (s[j - 1] == p[i - 2] || p[i - 2] == '.'));
       } else if (p[i - 1] == '?') {
         dp[i][j] = dp[i - 1][j - 1] || dp[i - 1][j] || dp[i][j - 1];
       }
--- src/algorithm/DNA_Analyzer.h
+++ src/algorithm/DNA_Analyzer.h
@@ -11,6 +11,8 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+#include <optional>
+#include <array>
 
 class DNA_Analyzer {
  public:
@@ -28,23 +30,22 @@ class DNA_Analyzer {
   void WindowAlgorithm(const std::filesystem::path& path);
 
  private:
-  const std::size_t rk_mod = 1e9 + 9;
-  const std::size_t rk_base = 31;
+  const std::array<char, 4> m_ABC = {'A', 'C', 'G', 'T'};
+  const std::size_t m_rk_mod = 1e9 + 9;
+  const std::size_t m_rk_base = 31;
+
+  std::array<int, 256> m_weights = {};
 
   std::string ReadFile(const std::filesystem::path& path);
   void CalculateMassHash(const std::string &src, std::vector<std::size_t> &hash, const std::size_t size, const std::vector<std::size_t> &exp);
+  std::pair<std::array<int, 3>, std::array<std::string, 2>> GetNWConfig(const std::filesystem::path& path);
+  std::vector<std::vector<int> > InitAlighmentMatrix(const std::array<int, 3> &conf, const std::array<std::string, 2> &seq, const std::vector<std::vector<int> > &score_mat);
+  std::array<std::string, 3> ComputeAlignedSequences(const std::array<int, 3> &conf, const std::array<std::string, 2> &seq, const std::vector<std::vector<int> > &mat, const std::vector<std::vector<int> > &score_mat);
+  std::vector<std::vector<int> > InitScoreMatrix(const std::array<int, 3> &conf, const std::size_t rows, const std::size_t cols);
+  void PrintNWResult(const std::array<std::string, 3> &alig, const int &score);
+  void InitializeWeights();
 
-
-
-
-  struct Score {
-    int match, mismatch, gap;
-    Score(int match, int mismatch, int gap)
-        : match(match), mismatch(mismatch), gap(gap) {}
-  };
-
-  int optimalScore(Score score, std::string s1, std::string s2);
-  std::pair<int, std::pair<std::string, std::string>> optimalAlignment(Score score, std::string s1, std::string s2);
+  
   bool isMatch(std::string s, std::string p);
   int kSimilarity(std::string s1, std::string s2);
   std::string minWindowSubstring(std::string s, std::string t);
